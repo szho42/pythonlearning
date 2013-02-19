af1ddab50df0b32a3c389b4d58cfb4ca30799290
szho42 <zhongsj52@gmail.com>
2013-02-19 12:52:49 +1100

    add boost-python sample

---
+++ boost-python/main.py
@@ -0,0 +1,10 @@
+#!/bin/bash/env python
+
+import hello_bye
+
+def main():
+    hello_bye.hello()
+    hello_bye.bye()
+
+if __name__ = "__main__":
+    main()
---
+++ boost-python/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+char const* hello()
+    {
+	return "hello world";
+    }
+
+void bye()
+    {
+        using namespace std;
+	cout << "bye" << endl;
+    }
